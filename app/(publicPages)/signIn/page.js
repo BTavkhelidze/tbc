3012295c5794458c569623aea29a62ae2e287eef
BTavkhelidze <beqatavkhelidze4@gmail.com>
2024-10-20 13:54:07 +0400

    feature signIn.js
    adding sign-In.js page

--- app/(publicPages)/signIn/page.js
+++ app/(publicPages)/signIn/page.js
@@ -1,6 +1,47 @@
+import Link from 'next/link';
 import styles from './signIn.module.css';
 export default function page() {
   return (
-    <div>sign in page</div>
-  )
+    <div className={styles.box}>
+      <div className={styles.box_header}>
+        <h5 className={styles.header_title}>LogIn</h5>
+      </div>
+      <div className={styles.input_box}>
+        <label></label>
+        <input
+          type='text'
+          className={styles.input_field}
+          placeholder='Email'
+          required
+        />
+      </div>
+      <div className={styles.input_box}>
+        <label></label>
+        <input
+          type='password'
+          className={styles.input_field}
+          placeholder='Password'
+          required
+        />
+      </div>
+      <div className={styles.forgot}>
+        <section>
+          <input type='checkbox' id={styles.check} />
+          <label for='check'>Remember me</label>
+        </section>
+        <section>
+          <Link href='/forgot-password'>Forgot password</Link>
+        </section>
+      </div>
+      <div className={styles.submit_wrapper}>
+        <button className={styles.submit_btn} id='submit' />
+        <label for='submit'>Sign In</label>
+      </div>
+      <div className={styles.sign_up_link}>
+        <p>
+          Don't have account ? <Link href='/signUp'>Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
